324620f89905223cc468ec698013cbe348feb926
Benjamin Geyer <brgeyer49@gmail.com>
2018-06-21 16:28:29 -0400

    Search model created and tested.

---
+++ src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from "axios"
+
+export default class Search {
+  constructor(query){
+    this.query = query
+  }
+  
+  async getResults() {
+    const key = "75d103387ce87f014da86a0e69e3fd1c"
+
+    try {
+      const res = await axios(`https://cors-anywhere.herokuapp.com/http://food2fork.com/api/search?key=${key}&q=${this.query}`)
+      this.result = res.data.recipes
+      console.log(this.result)
+    } catch (err) {
+      alert(err)
+    }
+  }  
+}
