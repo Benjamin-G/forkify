1c3cf2e423f5119bc4af34995c494acfd3e765bb
Benjamin Geyer <brgeyer49@gmail.com>
2018-07-09 18:34:49 -0400

    created a function to reduce a title to a single line

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -6,6 +6,25 @@ export const clearInput = () => { elements.searchInput.value = '' }
 
 export const clearResults = () => { elements.searchResList.innerHTML = '' }
 
+//Reduce the title length to a single line
+const limitRecipeTitle = (title, limit = 17) => {
+  const newTitle = []
+
+  if(title.length > limit){
+    title
+    .split(' ')
+    .reduce((acc, ele) => {
+      if(acc + ele.length <= limit){
+        newTitle.push(ele)
+      }
+      return acc + ele.length
+    }, 0)
+
+    return `${newTitle.join(' ')}...`
+  }
+  return title
+}
+
 const renderRecipe = recipe => {
   const markup = `<li>
     <a class="results__link" href="#${recipe.recipe_id}">
@@ -13,7 +32,7 @@ const renderRecipe = recipe => {
             <img src="${recipe.image_url}" alt="Test">
         </figure>
         <div class="results__data">
-            <h4 class="results__name">${recipe.title}</h4>
+            <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
             <p class="results__author">${recipe.publisher}</p>
         </div>
     </a>
