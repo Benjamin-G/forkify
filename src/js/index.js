5bf00c49d598f850d092a35c7f4caf2757410dba
Benjamin Geyer <brgeyer49@gmail.com>
2018-07-09 16:23:11 -0400

    Set up searchview for UI, base.js to contain DOM elements, added to controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,7 @@
 // Global app controller
 import Search from './models/Search'
+import * as searchView from './views/searchView'
+import { elements } from './views/base'
 
 /** Global state of the app
  * -search object
@@ -12,23 +14,26 @@ const state = {}
 
 const controlSearch = async () => {
   // get query TODO
-  const query = 'pasta'
+  const query = searchView.getInput()
+  console.log(query)
 
   if(query){
     // add search object to state
     state.search = new Search(query)
 
     // prepare UI for results
+    searchView.clearInput()
+    searchView.clearResults()
 
     // search for recipes
     await state.search.getResults()
 
     // render results in UI
-    console.log(state.search.result)
+    searchView.renderResults(state.search.result)
   }
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault()
   controlSearch()
 })
